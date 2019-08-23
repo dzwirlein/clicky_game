b7dd6815de54e626152f9e21e1490c5fcc3c077e
dzwirlein <47755855+dzwirlein@users.noreply.github.com>
2019-08-22 21:21:20 -0500

    Updates

---
+++ precache-manifest.1a06145cb98909880e36ac552eeb03dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d30c63aad7e39b7e1ac654a01f6758f0",
+    "url": "/clicky_game/index.html"
+  },
+  {
+    "revision": "95392a66748c2453e5da",
+    "url": "/clicky_game/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "5ea31f1f30a7ffdf207b",
+    "url": "/clicky_game/static/js/2.53b63a6f.chunk.js"
+  },
+  {
+    "revision": "95392a66748c2453e5da",
+    "url": "/clicky_game/static/js/main.b95afd6d.chunk.js"
+  },
+  {
+    "revision": "14f7946cacfc6dfc8762",
+    "url": "/clicky_game/static/js/runtime~main.38f4cfe3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky_game/static/media/logo.5d5d9eef.svg"
+  }
+]);
